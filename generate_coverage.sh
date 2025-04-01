65025b7f5f322a18b477868da3a998cb1321b732
Ilya-Klim <Klimasch2002@gmail.com>
2025-04-01 13:42:01 +0300

    -test

--- generate_coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Запуск тестов с измерением покрытия
-python -m pytest --cov=app tests/ --cov-report=html --cov-report=term
-
-# Вывод информации о покрытии
-echo "Coverage report generated in htmlcov/ directory"
